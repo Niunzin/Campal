91728c90aab7526e6a4b0f5f1c1a46aed52d0dff
Niunzin <niunzinumb@gmail.com>
2014-07-22 13:04:01 -0300

    Update campal.lua

--- campal.lua
+++ campal.lua
@@ -1,14 +1,23 @@
-local moduleSettings = {
+moduleSettings = {
 	["Admins"] = {"Niunzin"},
 	["Game Masters"] = {""},
 	["Mapcrews"] = {""},
 	["RoundTime"] = 600,
-	["Maplist"] = {},
+	["Maplist"] = {
+		["Rush"] = {
+			{ ["code"] = "@5263734", ["author"] = "Niunzin", ["name"] = "Tropical Islands" },
+		},
+		["Capture the Flag"] = {
+		},
+		["Team deathmatch"] = {
+		},
+	},
 	["Ban List"] = {},
 	["Languages"] = {
 		["br"] = {
 					["message_newGame"] = "Nova rodada!",
-					["message_newGame_rush"] = "Modo atual: <b>Rush</b>",
+					["message_newGame_2"] = "Modo de jogo: <font color=\"#F0A78E\">%s</font>",
+					["message_newGame_3"] = "Mapa <font color=\"#F0A78E\">%s</font>, por <font color=\"#F0A78E\">%s</font>.",
 				},
 	}
 }
@@ -29,7 +38,8 @@ local currentGame = {
 		}
 	},
 	["Mode"] = "Rush",
-	["Lastmode"] = nil
+	["Lastmode"] = nil,
+	["Map"] = nil,
 }
 
 local campalUtils = {}
@@ -38,17 +48,18 @@ local campalGet = tfm.get
 local campalUI = ui
 local campalMessageShow = 0
 local campalMessageQueue = {}
-local campalInit = tfm.exec.newGame(0)
 local tr = moduleSettings["Languages"]["br"]
+
+campalExec.disableAutoShaman(true)
+campalExec.disableAutoNewGame(true)
+campalExec.disableAutoScore(true)
+campalExec.disableAutoTimeLeft(true)
+campalExec.disableAfkDeath(true)
 
 function campalUtils.showMessage(Message, Target, Duration)
 	local Duration = (Duration * 2) or 10
 	if(campalMessageShow == 0) then
-		-- Shadow
-		campalUI.addTextArea(875, string.format("<p align=\"center\"><font size=\"31\" color=\"#000000\">%s</font></p>", Message), Target, 0, 30, 800, 48, nil, nil, 0, true)
-		-- Text
-		campalUI.addTextArea(876, string.format("<p align=\"center\"><font size=\"30\" color=\"#ff8000\">%s</font></p>", Message), Target, 0, 30, 800, 48, nil, nil, 0, true)
-		
+		campalUI.addTextArea(876, string.format("<p align=\"center\"><font size=\"30\" color=\"#E68D43\">%s</font></p>", Message), Target, 0, 30, 800, 48, nil, nil, 0, true)
 		campalMessageShow = Duration
 	else
 		local data = { ["message"] = Message, ["target"] = Target, ["duration"] = Duration }
@@ -59,7 +70,6 @@ end
 function eventLoop(currentTime, timeRemaining)
 	campalMessageShow = campalMessageShow - 1
 	if(campalMessageShow == 0) then
-		campalUI.removeTextArea(875, nil)
 		campalUI.removeTextArea(876, nil)
 		if(campalMessageQueue[1] ~= nil) then
 			local nextMessage = campalMessageQueue[1]
@@ -70,13 +80,22 @@ function eventLoop(currentTime, timeRemaining)
 	end
 end
 
-function eventNewGame()
+function campalInit()
 	if(currentGame["Lastmode"] == nil) then
 		currentGame["Mode"] = "Rush"
 	end
 	
 	if(currentGame["Mode"] == "Rush") then
-		campalUtils.showMessage(tr["message_newGame"], nil, 5)
-		campalUtils.showMessage(tr["message_newGame_rush"], nil, 5)
+		local mapList = moduleSettings["Maplist"]["Rush"]
+		local currentMap = mapList[math.random(#mapList)]
+		local mapName = currentMap["name"]
+		local mapCode = currentMap["code"]
+		local mapAuthor = currentMap["author"]
+		campalExec.newGame(mapCode)
+		campalUtils.showMessage(tr["message_newGame"], nil, 3)
+		campalUtils.showMessage(string.format(tr["message_newGame_2"], currentGame["Mode"]), nil, 3)
+		campalUtils.showMessage(string.format(tr["message_newGame_3"], mapName, mapAuthor), nil, 5)
 	end
 end
+
+campalInit()
