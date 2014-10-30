bcdc0226a4f913a4026cfe376e27ab18f5595cc3
Niunzin <niunzinumb@gmail.com>
2014-10-30 12:54:03 -0200

    Create translate.lua

---
+++ translate.lua
@@ -0,0 +1,68 @@
+	["default"] = {
+		["running"] = "Script running!",
+		["welcome"] = "Welcome <b>%s</b>, to Campal 2.0!",
+		["starting"] = "Starting in 5...",
+		["started"] = "<font color=\"#30BA76\">Started!</font>",
+		["your_team"] = "Your team: %s",
+		["c_blue"] = "<font color=\"#3D83FE\">BLUE</font>",
+		["c_red"] = "<font color=\"#FF3E3E\">RED</font>",
+		["team_balance"] = "Item limit per team: <B>%s</B>",
+		["no_players"] = "<R>GAME HASN'T STARTED! <N>Need <J>%s<N> more player(s).",
+		["greeting"] = "Welcome <B>%s</B>, to Campal 2.0! Type <VP>!help<N> for more information and <VP>!t [message]<N> to chat with your team.<br>Suggestions or bugs? <V>http://atelier801.com/section?f=286837&s=558185<N>.",
+		["botname"] = "Campal",
+		["help"] = [[<font size="16" color="#98E2EB">Campal Instructions</font>
+<font color="#E68D43">About</font>
+You need to get the cheese from the other team; if you try to catch the cheese of your own team you will be killed. When you finally get the cheese from the other team, you should return to your own territory and press <I>Space</I>.You need at least 4 mice in the room to play with you or the game won't start and you'll die either way (with you has to be 3 mice, without you 4). Press key 'down' or 's' to shoot a cannon.
+
+<font color="#E68D43">Commands</font>
+<font color="#EDCC8D">!t <B>[message]</B></font> - chat with your team
+<font color="#EDCC8D">!p <B>[player]</B></font> - player profile
+<font color="#EDCC8D">!list <B>[blue or red]</B></font> - list of members in a team</font>]],
+		["othercheese"] = "You need to get the cheese from the other team.",
+		["run"] = "Return to your territory and press <V>SPACE<N>.",
+		["no_team"] = "You don't have a team!",
+		["not_started"] = "Round not started!",
+		["gotcheese"] = "<B>%s</B> got a cheese!",
+		["red"] = "Red team",
+		["blue"] = "Blue team",
+		["fight"] = "<B>FIGHT!</B>",
+		["blue_won"] = "<CH><b>Blue</b> team won!",
+		["red_won"] = "<R><b>Red</b> team won!",
+		["draw"] = "<B>No winners</B>!",
+		["profile"] = [[<b><font size="16" color="#BABD2F">%s</font></b>
+» <font color="#30BA76">Wins :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Losses :</font> <font color="#009D9D">%s</font>
+
+» <font color="#30BA76">Deaths :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Points :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Gathered cheese :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Points (first) :</font> <font color="#009D9D">%s</font>
+
+» <font color="#30BA76">Spawned cannons :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Rounds :</font> <font color="#009D9D">%s</font>
+
+» <font color="#30BA76">Current cannon : </font><font color="#009D9D">%s</font>
+
+» <font color="#30BA76">Current skill : </font><font color="#009D9D">%s</font>]],
+		["first"] = "FIRST!",
+		["point"] = "%s scored a point!",
+		["end1"] = "<CH>Blue team <J>%s <N>x <J>%s <R>Red team",
+		["mapname"] = "<J>Campal Map",
+		["player_not_found"] = "<R>Player not found!",
+
+		-- shop
+		["scoredPoints"] = "Points: <font color=\"#BABD2F\">%s</font>",
+		["spawnedCannons"] = "Cannons: <font color=\"#BABD2F\">%s</font>",
+		["needCannons"] = "<font color=\"#BABD2F\">%s</font><font color=\"#c2c2da\">/%s</font>",
+		["needPoints"] = "<font color=\"#BABD2F\">%s</font><font color=\"#c2c2da\">/%s</font>",
+		["unlocked"] = "<font color=\"#2ECF73\">Use</font>",
+		["btnClose"] = "Close",
+		["btnCannon"] = "Cannons",
+		["btnAura"] = "Skills",
+		["usingcn"] = "<V>Using: <J>%s<V>.",
+		["cantbuy"] = "<p align=\"center\"><br><font color=\"#CB546B\">Item locked!</font><br></p>",
+		["404"] = "<p align=\"center\"><font size=\"20\" color=\"#CB546B\">No items available!</font></p>",
+		["itemlayout"] = "<a href=\"event:%s\"><BR><BR><BR><p align=\"center\">%s</p></a>",
+		["using"] = "<BR><BR><BR><p align=\"center\"><font color=\"#CB546B\"><a href=\"event:%s\">Remove</a></font></p>",
+		["typeusername"] = "Username: <i>(empty = your profile)</i>",
+	},
