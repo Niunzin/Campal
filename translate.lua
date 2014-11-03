a19122e533c2d0d001c2e0369971dc00ce3480af
Enginfener <ozan.forum@hotmail.com>
2014-11-03 10:22:56 +0200

    Update translate.lua

--- translate.lua
+++ translate.lua
@@ -1,68 +1,68 @@
 	["default"] = {
-		["running"] = "Script running!",
-		["welcome"] = "Welcome <b>%s</b>, to Campal 2.0!",
-		["starting"] = "Starting in 5...",
-		["started"] = "<font color=\"#30BA76\">Started!</font>",
-		["your_team"] = "Your team: %s",
-		["c_blue"] = "<font color=\"#3D83FE\">BLUE</font>",
-		["c_red"] = "<font color=\"#FF3E3E\">RED</font>",
-		["team_balance"] = "Item limit per team: <B>%s</B>",
-		["no_players"] = "<R>GAME HASN'T STARTED! <N>Need <J>%s<N> more player(s).",
-		["greeting"] = "Welcome <B>%s</B>, to Campal 2.0! Type <VP>!help<N> for more information and <VP>!t [message]<N> to chat with your team.<br>Suggestions or bugs? <V>http://atelier801.com/section?f=286837&s=558185<N>.",
+		["running"] = "Script Çalışıyor!",
+		["welcome"] = "Hoşgeldiniz <b>% s </ b>, Campal 2.0!",
+		["starting"] = "Başlatılıyor 5...",
+		["started"] = "<font color=\"#30BA76\">Başladı!</font>",
+		["your_team"] = "Senin Takımın: %s",
+		["c_blue"] = "<font color=\"#3D83FE\">MAVİ</font>",
+		["c_red"] = "<font color=\"#FF3E3E\">KIRMIZI</font>",
+		["team_balance"] = "Takım başına öğe sınırı: <B>%s</B>",
+		["no_players"] = "<R>OYUN BAŞLATILAMADI! <N>Daha  <J>%s<N> fazla oyuncuya ihtiyaç var.",
+		["greeting"] = "Campal 2.0 <B>%s</B>, Odasıba Hoş Geldiniz! Komutu Türleri <VP>!help<N> Daha fazla bilgi için ve <VP>!t [mesaj]<N> takım ekibi ile sohbet etmek için kullanabilirsin.<br>Öneriler veya hata?<V>http://atelier801.com/section?f=286837&s=558185<N>.",
 		["botname"] = "Campal",
-		["help"] = [[<font size="16" color="#98E2EB">Campal Instructions</font>
-<font color="#E68D43">About</font>
-You need to get the cheese from the other team; if you try to catch the cheese of your own team you will be killed. When you finally get the cheese from the other team, you should return to your own territory and press <I>Space</I>.You need at least 4 mice in the room to play with you or the game won't start and you'll die either way (with you has to be 3 mice, without you 4). Press key 'down' or 's' to shoot a cannon.
+		["help"] = [[<font size="16" color="#98E2EB">Campal Talimatlar</font>
+<font color="#E68D43">Hakkında</font>
+Senin diğer takımdan önce peynir alman gerekir; Eğer kendi takımından biri peynir almaya çalışırsa ölecektir. Sonuda diğer takım peynir almaya çalışacaktır ancak sen kendi bölgende <I>Boşluk</I> tuşuna basarak dönebilirsin.Senin veya diğer farelerin oyuna başlayabilmesi için 4 fareye ihtiyacı vardır, 2 tip öldürme seçeneği var "aşağı" tuşu yada  "s" tuşuna basarak ateş edebilirsin.
 
-<font color="#E68D43">Commands</font>
-<font color="#EDCC8D">!t <B>[message]</B></font> - chat with your team
-<font color="#EDCC8D">!p <B>[player]</B></font> - player profile
-<font color="#EDCC8D">!list <B>[blue or red]</B></font> - list of members in a team</font>]],
-		["othercheese"] = "You need to get the cheese from the other team.",
-		["run"] = "Return to your territory and press <V>SPACE<N>.",
-		["no_team"] = "You don't have a team!",
-		["not_started"] = "Round not started!",
-		["gotcheese"] = "<B>%s</B> got a cheese!",
-		["red"] = "Red team",
-		["blue"] = "Blue team",
-		["fight"] = "<B>FIGHT!</B>",
-		["blue_won"] = "<CH><b>Blue</b> team won!",
-		["red_won"] = "<R><b>Red</b> team won!",
-		["draw"] = "<B>No winners</B>!",
+<font color="#E68D43">Komutlar</font>
+<font color="#EDCC8D">!t <B>[mesaj]</B></font> - Takım sohbeti
+<font color="#EDCC8D">!p <B>[oyuncu isim]</B></font> - Oyuncu Profili
+<font color="#EDCC8D">!list <B>[mavi yada kırmızı]</B></font> - Bir takım üyelerin listesi</font>]],
+		["othercheese"] = "Senin diğer takımdan önce peyniri alman gerekir.",
+		["run"] = "Kendi bölgene dönmek için <V>BOŞLUK<N> tuşunu kullan.",
+		["no_team"] = "Takım Bulunamadı!",
+		["not_started"] = "Yeni Round Başlatılamadı",
+		["gotcheese"] = "<B>%s</B> bir peyniri var",
+		["red"] = "Kırmızı Takım",
+		["blue"] = "Mavi Takım",
+		["fight"] = "<B>BAŞLA!</B>",
+		["blue_won"] = "<CH><b>Mavi</b> takım kazandı!",
+		["red_won"] = "<R><b>Kırmızı</b> takım kazandın!",k
+		["draw"] = "<B>Kazanan Bulunamadı!</B>!",
 		["profile"] = [[<b><font size="16" color="#BABD2F">%s</font></b>
-» <font color="#30BA76">Wins :</font> <font color="#009D9D">%s</font>
-» <font color="#30BA76">Losses :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Galibiyet :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Kaybetmeler :</font> <font color="#009D9D">%s</font>
 
-» <font color="#30BA76">Deaths :</font> <font color="#009D9D">%s</font>
-» <font color="#30BA76">Points :</font> <font color="#009D9D">%s</font>
-» <font color="#30BA76">Gathered cheese :</font> <font color="#009D9D">%s</font>
-» <font color="#30BA76">Points (first) :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Ölümler :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Puanlar :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Toplanan peynir :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Puan ( Birincilik) :</font> <font color="#009D9D">%s</font>
 
-» <font color="#30BA76">Spawned cannons :</font> <font color="#009D9D">%s</font>
-» <font color="#30BA76">Rounds :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76"Ortaya Çıkarılan Gülleler  :</font> <font color="#009D9D">%s</font>
+» <font color="#30BA76">Roundlar :</font> <font color="#009D9D">%s</font>
 
-» <font color="#30BA76">Current cannon : </font><font color="#009D9D">%s</font>
+» <font color="#30BA76">Güncel Gülleler : </font><font color="#009D9D">%s</font>
 
-» <font color="#30BA76">Current skill : </font><font color="#009D9D">%s</font>]],
-		["first"] = "FIRST!",
-		["point"] = "%s scored a point!",
-		["end1"] = "<CH>Blue team <J>%s <N>x <J>%s <R>Red team",
-		["mapname"] = "<J>Campal Map",
-		["player_not_found"] = "<R>Player not found!",
+» <font color="#30BA76">Güncel Yetenekler : </font><font color="#009D9D">%s</font>]],
+		["first"] = "BİRİNCİ!",
+		["point"] = "%s bir noktaya puan attı!"
+		["end1"] = "<CH>Mavi Takım <J>%s <N>x <J>%s <R>Kırmızı Takım",
+		["mapname"] = "<J>Campal Harita",
+		["player_not_found"] = "<R>Oyuncu bulunamadı!",
 
 		-- shop
-		["scoredPoints"] = "Points: <font color=\"#BABD2F\">%s</font>",
-		["spawnedCannons"] = "Cannons: <font color=\"#BABD2F\">%s</font>",
+		["scoredPoints"] = "Puanlar: <font color=\"#BABD2F\">%s</font>",
+		["spawnedCannons"] = "Gülleler: <font color=\"#BABD2F\">%s</font>",
 		["needCannons"] = "<font color=\"#BABD2F\">%s</font><font color=\"#c2c2da\">/%s</font>",
 		["needPoints"] = "<font color=\"#BABD2F\">%s</font><font color=\"#c2c2da\">/%s</font>",
-		["unlocked"] = "<font color=\"#2ECF73\">Use</font>",
-		["btnClose"] = "Close",
-		["btnCannon"] = "Cannons",
-		["btnAura"] = "Skills",
-		["usingcn"] = "<V>Using: <J>%s<V>.",
-		["cantbuy"] = "<p align=\"center\"><br><font color=\"#CB546B\">Item locked!</font><br></p>",
-		["404"] = "<p align=\"center\"><font size=\"20\" color=\"#CB546B\">No items available!</font></p>",
+		["unlocked"] = "<font color=\"#2ECF73\">Kullan</font>",
+		["btnClose"] = "Kapat",
+		["btnCannon"] = "Gülleler",
+		["btnAura"] = "Yetenekler",
+		["usingcn"] = "<V>Kullanma: <J>%s<V>.",
+		["cantbuy"] = "<p align=\"center\"><br><font color=\"#CB546B\">ITEM kilitli!</font><br></p>",
+		["404"] = "<p align=\"center\"><font size=\"20\" color=\"#CB546B\">Öğe Kullanılamıyor!</font></p>",
 		["itemlayout"] = "<a href=\"event:%s\"><BR><BR><BR><p align=\"center\">%s</p></a>",
-		["using"] = "<BR><BR><BR><p align=\"center\"><font color=\"#CB546B\"><a href=\"event:%s\">Remove</a></font></p>",
-		["typeusername"] = "Username: <i>(empty = your profile)</i>",
+		["using"] = "<BR><BR><BR><p align=\"center\"><font color=\"#CB546B\"><a href=\"event:%s\">Sil</a></font></p>",
+		["typeusername"] = "Kullanıcı Adı: <i>(boş = senin kullanıcı adın)</i>",
 	},
